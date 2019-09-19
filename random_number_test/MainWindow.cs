9f39d0def372b10e14ce2d703e9cc37fa83e4fda
Jarosław Rauza <dynamio@gmail.com>
2019-09-19 20:08:22 +0200

    first version of project

---
+++ random_number_test/MainWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Timers;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private Timer timer;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        InitializeTimer(timer);
+    }
+
+    private void InitializeTimer(Timer timer)
+    {
+        timer = new Timer(1000);
+        timer.Elapsed += OnTimedEvent;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+        string newString = "";
+        Random random = new Random();
+
+        for (int i=0; i<60; i++)
+        {
+            newString += (char)random.Next(48, 91);
+        }
+
+        InsertText(textview.Buffer, newString);
+    }
+
+    private void OnSizeAllocated(object sender, SizeAllocatedArgs e)
+    {
+        textview.ScrollToIter(textview.Buffer.EndIter, 0, false, 0, 0);
+    }
+
+    private static void InsertText(TextBuffer textBuffer, string line)
+    {
+        TextIter textIter = textBuffer.EndIter;
+        textBuffer.Insert(ref textIter, line);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
